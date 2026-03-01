a0a58ba4d4bf6bf351e58f9b61b79368e1fc0cba
daycry <7590335+daycry@users.noreply.github.com>
2026-03-01 10:41:57 +0100

    Remove Guest authenticator from Auth config
    Delete the Guest authenticator import and its entry in the authenticators array in src/Config/Auth.php. This removes registration of the Guest authenticator (cleaning up unused/removed authenticator wiring).

--- src/Config/Auth.php
+++ src/Config/Auth.php
@@ -16,7 +16,6 @@ namespace Daycry\Auth\Config;
 use CodeIgniter\Config\BaseConfig;
 use Daycry\Auth\Authentication\Actions\Email2FA;
 use Daycry\Auth\Authentication\Authenticators\AccessToken;
-use Daycry\Auth\Authentication\Authenticators\Guest;
 use Daycry\Auth\Authentication\Authenticators\JWT;
 use Daycry\Auth\Authentication\Authenticators\Session;
 use Daycry\Auth\Authentication\JWT\Adapters\DaycryJWTAdapter;
@@ -99,7 +98,6 @@ class Auth extends BaseConfig
         'access_token' => AccessToken::class,
         'session'      => Session::class,
         'jwt'          => JWT::class,
-        'guest'        => Guest::class,
     ];
 
     /**
